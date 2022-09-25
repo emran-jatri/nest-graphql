696e5cab2be8186c525a56efa35ffe4c94c55375
ibnshayed <emranibnshayed@gmail.com>
2022-09-25 20:34:00 +0600

    graphql error and response format added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 
 @Module({
 	imports: [
@@ -14,6 +14,39 @@ import { join } from 'path';
 			driver: ApolloDriver,
 			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
 			sortSchema: true,
+			formatResponse(response, requestContext) {
+				let message = "SUCCESS"
+				let statusCode = 200
+
+				if (response?.errors) {
+					// console.log("🚀 ~ file: app.module.ts ~ line 23 ~ formatResponse ~ response?.errors", response?.errors)
+					// @ts-ignore
+					message = response.errors[0]?.extensions?.response?.message || response.errors[0]?.message || "ERROR"
+					// @ts-ignore
+					statusCode = response.errors[0]?.extensions?.response?.statusCode || 500
+					response.errors = undefined
+				}
+
+				// console.log("🚀 ~ file: app.module.ts ~ line 31 ~ formatResponse ~ response.data", response.data)
+				response.data = {
+					message,
+					statusCode,
+					...response.data,
+				}
+				// response.extensions = {...response.extensions, test2: 'Property to test if shown in the FrontEnd'}
+				// let responseData = {
+				// 	...response,
+				// 	test: 'Property to test if shown in the FrontEnd',
+				// }
+				// console.log("🚀 ~ file: app.module.ts ~ line 19 ~ formatResponse ~ responseData", responseData)
+     		return response
+			},
+			// formatError: (error: GraphQLError) => {
+			// 	const graphQLFormattedError: GraphQLFormattedError = {
+      //     message: error?.extensions?.response?.message || error?.message,
+      //   };
+      //   return graphQLFormattedError;
+      // },
     }),
 		UserModule
 	],
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -3,6 +3,8 @@ import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { response } from 'express';
+import { NotFoundException } from '@nestjs/common';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -15,13 +17,18 @@ export class UserResolver {
 
   @Query(() => [User], { name: 'users' })
   findAll() {
-    console.log("🚀 ~ file: user.resolver.ts ~ line 18 ~ UserResolver ~ findAll ~ findAll")
     return this.userService.findAll();
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.userService.findOne(id);
+	findOne(@Args('id', { type: () => Int }) id: number) {
+		const user = this.userService.findOne(id)
+
+		if (!user) {
+			throw new NotFoundException("User not found!")
+		}
+		
+		return user;
   }
 
   @Mutation(() => User)
